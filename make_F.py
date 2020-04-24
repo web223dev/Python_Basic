0dcb7f208b1ffb92ecf24099d05acf79d368fc5f
web223dev <ruslanus866@outlook.com>
2020-04-24 23:06:28 +0800

    make F

---
+++ make_F.py
@@ -0,0 +1,9 @@
+letters = [5, 2, 5, 2, 2]
+for letter in letters:
+      print("*" * letter)
+      
+# for letter in letters:
+#       aaa = ''
+#       for x in range(letter):
+#             aaa = aaa + "*"
+#       print(aaa)
