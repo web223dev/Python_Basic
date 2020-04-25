3f56fe8ae32f63a46d9240f939a482df5c2a03c2
web223dev <ruslanus866@outlook.com>
2020-04-25 15:20:38 +0800

    update convert emoticon

--- convert_emoticon.py
+++ convert_emoticon.py
@@ -1,10 +1,13 @@
+def emoji(text):
+    word = text.split(' ')
+    emoji = {
+        ":)": "🤗",
+        ":(": "☹"
+    }
+    output = ""
+    for w in word:
+        output += emoji.get(w, w) + ' '
+    return output
+
 text = input(">")
-word = text.split(' ')
-emoji = {
-    ":)": "🤗",
-    ":(": "☹"
-}
-output = ""
-for w in word:
-    output += emoji.get(w, w) + ' '
-print(output)
+print(emoji(text))
