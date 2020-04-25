17f16bccd7f7d168b367fc8b0fc15ccfd2c33518
web223dev <ruslanus866@outlook.com>
2020-04-25 10:40:29 +0800

    Translate Phone number

---
+++ translate_phonenumber.py
@@ -0,0 +1,17 @@
+translate_number = {
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four"
+}
+
+number = input("Phone: ")
+ch = ""
+for n in number:
+    ch += translate_number.get(n, "!") + " "
+print(ch)
+
+# My Method
+# number = input("Phone: ")
+# for n in number:
+#     print(translate_number[n])
